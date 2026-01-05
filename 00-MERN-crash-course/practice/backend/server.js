3c4fc299d9557fe8b25f631e5df8c89f8809509c
Ahmad Hassan <ahmadhassan.dev@outlook.com>
2026-01-05 18:52:07 +0500

    practice on postman for frontend simulation

--- 00-MERN-crash-course/practice/backend/server.js
+++ 00-MERN-crash-course/practice/backend/server.js
@@ -1,14 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products",(req, res) => {
-  res.send("Server is ready");
-})
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+if (!product.name || !product.price || !product.image) {
+  return res.status(400).json({ success : false, message: "Please provide all fields." });
+}
+const newProduct = new product(product)
+try {
+  await newProduct.save();
+  res.status(201).json({ success: true, data: newProduct });
+} catch (error) {
+  console.error("Error in creating product:", error.message);
+  res.status(500).json({ success: false, message: "Server Error" });      
+}
+});
 
 const PORT = process.env.PORT || 5001;
 
@@ -16,4 +30,3 @@ app.listen(PORT, () => {
   connectDB();
   console.log(`Server started at http://localhost:${PORT}`);
 });
-
