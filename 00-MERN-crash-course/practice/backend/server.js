b43a115242f2a8be5f0941a78a734eafb5552a37
Ahmad Hassan <ahmadhassan.dev@outlook.com>
2026-01-06 17:05:57 +0500

    Trouble shooted postman related bug

--- 00-MERN-crash-course/practice/backend/server.js
+++ 00-MERN-crash-course/practice/backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import product from "./models/product.model.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -10,18 +10,22 @@ const app = express();
 app.use(express.json());
 
 app.post("/api/products", async (req, res) => {
-  const product = req.body;
-if (!product.name || !product.price || !product.image) {
-  return res.status(400).json({ success : false, message: "Please provide all fields." });
-}
-const newProduct = new product(product)
-try {
-  await newProduct.save();
-  res.status(201).json({ success: true, data: newProduct });
-} catch (error) {
-  console.error("Error in creating product:", error.message);
-  res.status(500).json({ success: false, message: "Server Error" });      
-}
+  const { name, price, image } = req.body;
+
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields." });
+  }
+
+  try {
+    const newProduct = new Product({ name, price, image });
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in creating product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 const PORT = process.env.PORT || 5001;
