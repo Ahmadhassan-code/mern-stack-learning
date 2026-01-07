911b07ad8c7e008f0d0830c0cd53bafa920a76a1
Ahmad Hassan <ahmadhassan.dev@outlook.com>
2026-01-07 23:08:13 +0500

    Practice CRUD Operation

--- 00-MERN-crash-course/practice/backend/server.js
+++ 00-MERN-crash-course/practice/backend/server.js
@@ -2,12 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import { mongo } from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 app.post("/api/products", async (req, res) => {
   const { name, price, image } = req.body;
@@ -27,6 +38,34 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 });
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongo.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid Product Id" });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.error("Error in updating product:", error.message);
+    res.status(404).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  
+  try {
+    await Product.findByIdAndDelete(id); 
+    res.status(200).json({ success: true, message:"Product Deleted" });
+  } catch (error) {
+    console.error("Error in deleting product:", error.message);
+    res.status(404).json({ success: false, message: "Product Not Found" });
+  }
+})
 
 const PORT = process.env.PORT || 5001;
 
